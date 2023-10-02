f6b8ab9f24abaccb4e98b78e99630d8261660336
Jake Walker <jake.walker@rakuten.com>
2023-10-02 19:51:18 +0100

    Wired in read_bytes. Added test for reading from file

--- ccwc.py
+++ ccwc.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print('4')
+from src.wc import wc
+import sys
+
+wordcount = wc()
+
+arguments = sys.argv
+
+arg2 = arguments[2]
+
+bytes = wordcount.read_bytes(arg2)
+
+print(bytes)
--- tests/test_ccwc.py
+++ tests/test_ccwc.py
@@ -3,9 +3,24 @@ import subprocess
 
 class TestCCWC(unittest.TestCase):
     
-    def test_read_arguments_from_terminal(self):
+    def test_string_passed_as_argument_returns_bytes_length(self):
 
         process = subprocess.Popen(["./ccwc.py", "-c", "jake"], stdout=subprocess.PIPE, text=True)
+        stdout, stderr = process.communicate()
+
+        self.assertEqual(stdout.strip(), '4')
+
+    
+    def test_string_passed_as_argument_returns_bytes_length_2(self):
+
+        process = subprocess.Popen(["./ccwc.py", "-c", "letter"], stdout=subprocess.PIPE, text=True)
+        stdout, stderr = process.communicate()
+
+        self.assertEqual(stdout.strip(), '6')
+
+    def test_text_file_passed_as_argument_returns_bytes_length(self):
+
+        process = subprocess.Popen(["./ccwc.py", "-c", "text.txt"], stdout=subprocess.PIPE, text=True)
         stdout, stderr = process.communicate()
 
         self.assertEqual(stdout.strip(), '4')
