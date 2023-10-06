a2a06ba7334bcdc6793c97b24be767670ccf677f
Jake Walker <jake.walker@rakuten.com>
2023-10-06 20:08:52 +0100

    Using relative path for test data

--- tests/test_ccwc.py
+++ tests/test_ccwc.py
@@ -4,7 +4,7 @@ import subprocess
 class TestCCWC(unittest.TestCase):
     
     def test_ccwc_returns_bytes_length_and_test_file_full_run(self):
-        process = subprocess.Popen(["./ccwc.py", "-c", "/Users/jake.walker/personal-development/wc/text.txt"], stdout=subprocess.PIPE, text=True)
+        process = subprocess.Popen(["./ccwc.py", "-c", "text.txt"], stdout=subprocess.PIPE, text=True)
         stdout, stderr = process.communicate()
 
-        self.assertEqual(stdout.strip(), "335041 /Users/jake.walker/personal-development/wc/text.txt")
+        self.assertEqual(stdout.strip(), "335041 text.txt")
