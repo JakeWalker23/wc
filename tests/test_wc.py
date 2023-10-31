1d331c318a227b6773f346c0597cc0546c385fa8
Jake Walker <jake.walker@rakuten.com>
2023-10-31 21:08:32 +0000

    Added failing test for -m read characters from file

--- tests/test_wc.py
+++ tests/test_wc.py
@@ -25,13 +25,6 @@ class TestWC(unittest.TestCase):
         result = wordcount.read_lines_from_file(filepath)
 
         self.assertEqual(result, "1 tests/data/text.txt")
-    
-    def test_fileNotFound_exception_raised_when_file_does_not_exist_read_lines(self):
-        wordcount = wc()
-        filepath = 'does/not/exist.txt'
-
-        with self.assertRaises(FileNotFoundError):
-            wordcount.read_lines_from_file(filepath)
 
     def test_read_word_count_from_file(self):
         wordcount = wc()
@@ -40,3 +33,18 @@ class TestWC(unittest.TestCase):
         result = wordcount.read_words_from_file(filepath)
 
         self.assertEqual(result, "1 tests/data/text.txt")
+
+    def test_read_characters_from_file(self):
+        wordcount = wc()
+        filepath = 'tests/data/text.txt'
+
+        result = wordcount.read_characters_from_file(filepath)
+
+        self.assertEqual(result, "4 tests/data/text.txt")
+    
+    def test_fileNotFound_exception_raised_when_file_does_not_exist_read_lines(self):
+        wordcount = wc()
+        filepath = 'does/not/exist.txt'
+
+        with self.assertRaises(FileNotFoundError):
+            wordcount.read_lines_from_file(filepath)
